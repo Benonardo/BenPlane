4fc6814caa89428e0848cd631c17a254cc6a7ff3
Benonardo <benonardo@icloud.com>
2021-12-24 10:58:20 +0100

    Upstreamed to CloudPlane

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,8 @@
 plugins {
-    idea
-    java
+    `java-library`
 
-    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.12"
-}
-
-allprojects {
-    apply(plugin = "java")
-    apply(plugin = "maven-publish")
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(16))
-        }
-    }
-
-    tasks.withType<JavaCompile> {
-        options.isFork = true; options.isIncremental = true; options.encoding =
-        Charsets.UTF_8.name(); options.release.set(16)
-    }
-}
-
-subprojects {
-
-    tasks.withType<Javadoc> {
-        options.encoding = Charsets.UTF_8.name()
-    }
-
-    tasks.withType<ProcessResources> {
-        filteringCharset = Charsets.UTF_8.name()
-    }
-
-    repositories {
-        mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://nexus.velocitypowered.com/repository/velocity-artifacts-snapshots/")
-        maven("https://jitpack.io")
-        maven("https://libraries.minecraft.net")
-    }
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("io.papermc.paperweight.patcher") version "1.3.1"
 }
 
 repositories {
@@ -51,28 +10,66 @@ repositories {
     maven("https://papermc.io/repo/repository/maven-public/") {
         content { onlyForConfigurations("paperclip") }
     }
-    maven("https://maven.quiltmc.org/repository/release/") {
-        content { onlyForConfigurations("remapper") }
-    }
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
-    paperclip("io.papermc:paperclip:2.0.1")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
+    remapper("net.fabricmc:tiny-remapper:0.7.0:fat")
+    paperclip("io.papermc:paperclip:3.0.2")
+}
+
+subprojects {
+
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    repositories {
+        mavenCentral()
+
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+    }
+
 }
 
 paperweight {
     serverProject.set(project(":BenPlane-Server"))
 
-    useStandardUpstream("purpur") {
-        url.set(github("pl3xgaming", "Purpur"))
-        ref.set(providers.gradleProperty("purpurRef"))
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+    useStandardUpstream("cloudplane") {
+        url.set(github("Cloudcraftprojects", "CloudPlane"))
+        ref.set(providers.gradleProperty("cloudplaneRef"))
 
         withStandardPatcher {
-            baseName("Purpur")
+            apiSourceDirPath.set("CloudPlane-API")
+            serverSourceDirPath.set("CloudPlane-Server")
 
-            remapRepo.set("https://maven.fabricmc.net/")
-            decompileRepo.set("https://files.minecraftforge.net/maven/")
+            apiPatchDir.set(layout.projectDirectory.dir("patches${File.separator}api"))
+            serverPatchDir.set(layout.projectDirectory.dir("patches${File.separator}server"))
 
             apiOutputDir.set(layout.projectDirectory.dir("BenPlane-API"))
             serverOutputDir.set(layout.projectDirectory.dir("BenPlane-Server"))
